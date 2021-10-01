dec083cbf9c30d66020320bdccf007eb1e64343e
Ben Seward <benseward@Bens-MacBook-Pro.local>
2021-10-01 15:54:56 +0100

    save point

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber";
-import { Sky } from "@react-three/drei";
+import { Sky, Stars } from "@react-three/drei";
 import "./App.css";
 import Ground from "./Ground";
 import { Physics } from "@react-three/cannon";
@@ -9,10 +9,12 @@ import Player from "./Player";
 function App() {
   return (
     <div style={{ width: "100%", height: "100%" }}>
-      <Canvas>
-        <Sky sunPosition={new Vector3(1000, 10, 1000)} />
+      <Canvas style={{ backgroundColor: "#000000" }}>
+        {/* <Sky distance={45000} sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25} /> */}
+        <Sky distance={6000} turbidity={8} rayleigh={4} mieCoefficient={0.02} mieDirectionalG={0.6} inclination={0.5} azimuth={0.25} />
+
         <ambientLight intensity={0.3} />
-        <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
+        <pointLight castShadow intensity={0.8} position={[1000, 1000, 1000]} />
 
         <Physics gravity={[0, -30, 0]}>
           <Ground />
--- src/Player.tsx
+++ src/Player.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import { useSphere, useTrimesh } from "@react-three/cannon";
+import { useBox, useCompoundBody, useSphere, useTrimesh } from "@react-three/cannon";
 import { useThree, useFrame } from "@react-three/fiber";
 import { Vector3 } from "three";
 import usePlayerControls from "./usePlayerControls";
@@ -9,51 +9,62 @@ const SPEED = 5;
 
 export const Player = (props: any) => {
   const { moveForward, moveBackward, moveLeft, moveRight, jump } = usePlayerControls();
-  const [ref, api] = useTrimesh(() => ({
+  const [ref, api] = useSphere(() => ({
     mass: 1,
+    velocity: 2,
     type: "Dynamic",
     position: [0, 10, 0],
     ...props,
   }));
 
-  //   const velocity = useRef<any>([0, 0, 0]);
-  //   useEffect(() => {
-  //     api.velocity.subscribe((v) => (velocity.current = v));
-  //   }, [api.velocity]);
+  const velocity = useRef<any>([0, 10, 0]);
+  const currentPosition = useRef<any>([0, 0, 0]);
+
+  useEffect(() => {
+    api.velocity.subscribe((v) => (velocity.current = v));
+    api.position.subscribe((v) => (currentPosition.current = v));
+  }, [api.velocity, api.position]);
 
   useFrame(() => {
     if (!ref.current || !ref?.current?.position?.length) {
       return;
     }
 
-    //   const direction = new Vector3();
-    //   const frontVector = new Vector3(0, 0, Number(moveBackward) - Number(moveForward));
-    //   const sideVector = new Vector3(Number(moveLeft) - Number(moveRight), 0, 0);
-    //   direction.subVectors(frontVector, sideVector).normalize().multiplyScalar(SPEED);
-    //   api.velocity.set(direction.x, velocity.current[1], direction.z);
-    //   if (jump && Math.abs(velocity.current[1].toFixed(2)) < 0.05) {
-    //     api.velocity.set(velocity.current[0], 10, velocity.current[2]);
-    //   }
+    const direction = new Vector3();
+    const frontVector = new Vector3(0, 0, Number(moveBackward) - Number(moveForward));
+    const sideVector = new Vector3(Number(moveLeft) - Number(moveRight), 0, 0);
+    // direction.subVectors(frontVector, sideVector).normalize().multiplyScalar(SPEED);
 
-    const currentXPosition = ref?.current?.position.x;
-    const currentYPosition = ref?.current?.position.x;
-    const currentZPosition = ref?.current?.position.z;
 
-    console.log(currentXPosition);
+    api.velocity.set(direction.x, velocity.current[1], direction.z);
 
-    const moveOnXAxis = (Number(moveRight) - Number(moveLeft))+currentXPosition;
-    const moveOnZAxis = (Number(moveBackward) - Number(moveForward))+currentZPosition;
+    if (jump && Math.abs(velocity.current[1].toFixed(2)) < 0.05) {
+      console.log("aaaaa")
+      api.velocity.set(velocity.current[0], 10, velocity.current[2]);
+    }
 
-    ref.current.position.set(moveOnXAxis, 5, moveOnZAxis);
+    //new working code
+    // const currentXPosition = ref?.current?.position.x;
+    // const currentYPosition = ref?.current?.position.y;
+    // const currentZPosition = ref?.current?.position.z;
+
+    // console.log(currentZPosition)
+
+    // const moveOnXAxis = (Number(moveRight) - Number(moveLeft))+currentXPosition;
+    // const moveOnYAxis = jump ?  10 : 5;
+    // const moveOnZAxis = (Number(moveBackward) - Number(moveForward))+currentZPosition;
+
+    // api.position.set(moveOnXAxis, moveOnYAxis, moveOnZAxis);
   });
 
+
   return (
-    <group ref={ref}>
-      <PerspectiveCamera makeDefault position={[0, 25, 45]} rotation={[-0.55, 0, 0]} />
+    <mesh ref={ref}>
+      <PerspectiveCamera makeDefault position={[0, 25, 45]} rotation={[-0.25, 0, 0]} />
       <RoundedBox args={[5, 10, 5]} radius={0.5} smoothness={4}>
         <meshPhongMaterial attach="material" color="#f3f3f3" wireframe />
       </RoundedBox>
-    </group>
+    </mesh>
   );
 };
 
